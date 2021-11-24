4dfcc34a3c61c55358ca4d612840ccbe827d16eb
Anshika0287 <80511722+Anshika0287@users.noreply.github.com>
2021-11-24 15:23:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacToe from './Tic-Tac-Toe/Tic-Tac-Toe';
+
+function App() {
+	return (
+		<div className='App'>
+			<TicTacToe />
+		</div>
+	);
+}
+
+export default App;
